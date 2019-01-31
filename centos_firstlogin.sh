852bd0bf93e80080badac8ce94c5f720949fe056
SeanSingh <29295926+SeanSingh@users.noreply.github.com>
2019-01-31 14:44:46 -0600

    removed wget command.

--- centos_firstlogin.sh
+++ centos_firstlogin.sh
@@ -1,7 +1,6 @@
 username="$(whoami)"
 id_rsa="$(cat ~/.ssh/$pubkey)"
 pubkey="$(curl -L https://raw.githubusercontent.com/Open-NGO/SecureExamples/master/ssh_pubkey_tf)"
-pubkey="$(wget https://raw.githubusercontent.com/Open-NGO/SecureExamples/master/ssh_pubkey_tf-q -O -)"
 
 sudo echo "Compression no" >> /etc/ssh/sshd_config
 sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
@@ -17,5 +16,6 @@ sudo sed -i 's/#PermitTunnel no/PermitTunnel yes/g' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
 sudo mkdir -p ~/.ssh && chmod 700 ~/.ssh
-echo "$pubkey" >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+echo "$pubkey" >> ~/.ssh/authorized_keys
+sudo chmod 600 ~/.ssh/authorized_keys
 # mkdir -p $HOME/.ssh && chmod 700 $HOME/.ssh && echo "$pubkey" >> $HOME/.ssh/authorized_keys && chmod 600 $HOME/.ssh/authorized_keys"
