10366a899f6d385d38b51f959302af1937916263
SeanSingh <29295926+SeanSingh@users.noreply.github.com>
2019-01-31 18:38:25 -0600

    pre-reqs for installing ansible with python3.

---
+++ install_ansible_centos.sh
@@ -0,0 +1,6 @@
+#For Ansible on Python 3
+
+sudo yum install epel-release -y
+sudo yum install python36-devel python36-pip openssl-devel -y
+yum group install "Development Tools"
+pip3.6 install ansible
